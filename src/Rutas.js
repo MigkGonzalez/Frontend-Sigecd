06900f7b57de6e278a2d0b8568d6243452e8043e
MigkGonzalez <mfgonzaleza@unal.edu.co>
2021-12-03 12:49:33 -0500

    Desarrollo de diagramacion frontend, creacion de vista entrenador con botones para activar la edicion de horarios, equipos y listar deportistas

--- src/Rutas.js
+++ src/Rutas.js
@@ -6,6 +6,7 @@ import Login from './components/auth/login/Login';
 import ErrorPage from './components/ErrorPage';
 import Register from "./components/Register";
 import Footer from './components/Footer';
+import VistaEntrenador from "./components/VistaEntrenador";
 
 class Rutas extends Component{
 
@@ -19,8 +20,10 @@ class Rutas extends Component{
             <Routes>
                 <Route exact path="/" element={<Home />}/>  
                 <Route exact path="/login" element={<Login />}/>  
-                <Route exact path="/register" element={<Register />}/>  
+                <Route exact path="/register" element={<Register />}/> 
+                <Route exact path="/entrenador" element={<VistaEntrenador />}/> 
                 <Route path="*" element={<ErrorPage />}/>
+                
            </Routes>
            <div className= "clearFix"></div>
            </div>
